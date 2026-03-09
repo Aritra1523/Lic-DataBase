304659ac8123f9d692811413b965aab82216cb9a
Arita Das <darita385@gmail.com>
2026-03-09 20:13:40 +0530

    filupload  button updatde

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,3 +1,67 @@
+// import { useState } from "react";
+// import { storage, BUCKET_ID } from "../appwrite/config";
+// import { ID } from "appwrite";
+
+// function FileUpload({ setFileId }) {
+
+//   const [status, setStatus] = useState("");
+
+//   const upload = async (selectedFile) => {
+
+//     if (!selectedFile) {
+//       setStatus("Please select a file first");
+//       return;
+//     }
+
+//     try {
+
+//       setStatus("Uploading...");
+
+//       const res = await storage.createFile(
+//         BUCKET_ID,
+//         ID.unique(),
+//         selectedFile
+//       );
+
+//       setFileId(res.$id);
+
+//       setStatus("Upload Successful ✅");
+
+//     } catch (err) {
+
+//       console.log(err);
+//       alert(err.message);
+      
+//       setStatus("Upload Failed ❌");
+
+//     }
+
+//   };
+
+//   return (
+
+//     <div>
+
+//       <input
+//         type="file"
+//         accept=".pdf,image/*"
+//         onChange={(e) => upload(e.target.files[0])}
+//       />
+
+//       <p>{status}</p>
+
+//     </div>
+
+//   );
+
+// }
+
+// export default FileUpload;
+
+
+
+
+
 import { useState } from "react";
 import { storage, BUCKET_ID } from "../appwrite/config";
 import { ID } from "appwrite";
@@ -6,7 +70,9 @@ function FileUpload({ setFileId }) {
 
   const [status, setStatus] = useState("");
 
-  const upload = async (selectedFile) => {
+  const upload = async (e) => {
+
+    const selectedFile = e.target.files[0];
 
     if (!selectedFile) {
       setStatus("Please select a file first");
@@ -25,14 +91,14 @@ function FileUpload({ setFileId }) {
 
       setFileId(res.$id);
 
-      setStatus("Upload Successful ✅");
+      setStatus(`${selectedFile.name} uploaded successfully ✅`);
 
     } catch (err) {
 
       console.log(err);
-      alert(err.message);
-      alert(err)
       setStatus("Upload Failed ❌");
+
+      alert(err.message);
 
     }
 
@@ -45,7 +111,8 @@ function FileUpload({ setFileId }) {
       <input
         type="file"
         accept=".pdf,image/*"
-        onChange={(e) => upload(e.target.files[0])}
+        capture="environment"
+        onChange={upload}
       />
 
       <p>{status}</p>
